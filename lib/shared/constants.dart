2d66d30c8adeda39b930d4f8746ff5a73c79ea08
unknown <kebeseydina56@gmail.com>
2022-10-24 02:58:18 +0000

    Jokoo app chat

---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:34203239056:android:aa45bd062b294f5eb38e15";
+  static String apiKey = "AIzaSyDahwzPPHTVVC4b9-cmQVV7LEHbLJn44pE";
+  static String messagingSenderId = "34203239056";
+  static String projectId = "chat-app-xarala";
+  static String authDomain = "chat-app-xarala.firebaseapp.com";
+  final primaryColor = const Color(0xff223ced);
+}
